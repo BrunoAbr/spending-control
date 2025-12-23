1bd8098fe8a14a0425803766087cc8744aaf6960
Bruno <bruno.abreuxyz@gmail.com>
2025-12-22 22:39:48 -0300

    feat: add email-based password recovery and improve login experience with loading state

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,22 +9,37 @@ function onChangePassword(){
 }
 
 function login() {
+    showLoading();
     firebase.auth().signInWithEmailAndPassword(form.email().value, form.password().value).then(response => {
-        window.location.href = "pages/home/home.html"
+        hideLoading();
+        window.location.href = "pages/home/home.html";
     }).catch(error => {
+        hideLoading();
+        alert(getErrorMessage(error));
+    });
+}
+
+function recoverPassword(){
+    showLoading()
+    firebase.auth().sendPasswordResetEmail(form.email().value).then(() => {
+        hideLoading();
+        alert("E-mail de recuperação de senha foi enviado!")
+    }).catch(error => {
+        hideLoading();
         alert(getErrorMessage(error))
     });
 }
 
 function getErrorMessage(error){
     if (error.code == "auth/invalid-credential") {
-        return "Usuário não encontrado";
+        return "Usuário/Senha não encontrado";
     }
     return error.message;
 }
 
 function register() {
-    window.location.href = "pages/register/register.html"
+    showLoading();
+    window.location.href = "pages/register/register.html";
 }
 
 function isEmailValid() {
