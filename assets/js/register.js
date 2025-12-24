061c1e58b01084501eb5dfa606ada961d4422013
Bruno <bruno.abreuxyz@gmail.com>
2025-12-24 00:06:37 -0300

    feat: add registration screen functionality

--- assets/js/register.js
+++ assets/js/register.js
@@ -18,6 +18,31 @@ function onChangeConfirmPassword() {
     toggleRegisterButtonDisable();
 }
 
+function register(){
+    showLoading();
+    const email = form.email().value;
+    const password = form.password().value;
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(() => { 
+        hideLoading();
+        window.location.href = "../../pages/home/home.html";
+    }).catch(error => {
+        hideLoading();
+        alert(getErrorMessage(error));
+    })
+}
+
+function getErrorMessage(error) {
+    if (error.code == "auth/email-already-in-use"){
+        return "Esse e-mail já possui uma conta vinculada a ele."
+    }
+    return error.message;
+}
+
+function backToLoginPage() {
+    showLoading();
+    window.location.href = "../../index.html";
+}
+
 function validatePasswordsMatch() {
     const password = form.password().value;
     const confirmPassword = form.confirmPassword().value;
