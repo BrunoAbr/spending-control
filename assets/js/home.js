4dd0a20012182a623d578f316f5d14f1f8d2b7a0
Bruno <bruno.abreuxyz@gmail.com>
2026-01-03 20:59:08 -0300

    feat: add a way to edit the transactions

--- assets/js/home.js
+++ assets/js/home.js
@@ -20,7 +20,10 @@ function findTransactions(user) {
     showLoading();
     firebase.firestore().collection('transactions').where('user.uid', '==', user.uid).orderBy('date', 'desc').get().then(snapshot => {
        hideLoading();
-        const transactions = snapshot.docs.map(doc => doc.data());
+       const transactions = snapshot.docs.map(doc => ({
+            ...doc.data(),
+            uid: doc.id
+        }));
        addTransactionsToScreen(transactions);
     }).catch(error => {
         hideLoading();
@@ -36,6 +39,10 @@ function addTransactionsToScreen(transactions) {
     transactions.forEach(transaction => {
         const li = document.createElement('li');
         li.classList.add(transaction.type);
+        li.addEventListener('click', () => {
+            console.log(transactions)
+            window.location.href = "../transaction/transaction.html?uid=" + transaction.uid;
+        })
 
         const date = document.createElement('p');
         date.innerHTML = formatDate(transaction.date);
--- assets/js/transaction.js
+++ assets/js/transaction.js
@@ -1,16 +1,88 @@
-function saveTransaction() {
+if (!isNewTransaction()) {
+    const uid = getTransactionUid();
+    findTransactionByUid(uid);
+}
+
+function getTransactionUid() {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('uid');
+}
+
+function isNewTransaction() {
+    return getTransactionUid() ? false : true;
+}
+
+function findTransactionByUid(uid) {
     showLoading();
 
-    const transaction = createTransaction();
+    firebase.firestore().collection('transactions').doc(uid).get().then(doc => {
+        hideLoading();
+        if (doc.exists) {
+            fillTransactionScreen(doc.data());
+            toggleSaveButtonDisable();
+        } else {
+            alert("Documento não encontrado");
+            window.location.href = "../home/home.html";
+        }
+    }). catch(() => {
+        hideLoading();
+        alert("Erro ao recuperar documento");
+        window.location.href = "../home/home.html";
+    });
+}
 
+function fillTransactionScreen(transaction) {
+    if (transaction.type == "expense") {
+        form.typeExpense().checked = true;
+    } else {
+        form.typeIncome().checked = true;
+    }
+
+    form.date().value = transaction.date;
+    form.currency().value = transaction.money.currency;
+    form.value().value = transaction.money.value;
+    form.transactionType().value = transaction.transactionType;
+
+    if (transaction.description) {
+        form.description().value = transaction.description;
+    }
+}
+
+
+
+function saveTransaction() {
+    
+
+    const transaction = createTransaction();
+    if (isNewTransaction()) {
+        save(transaction);
+    } else {
+        update(transaction);
+    }
+    
+    
+}
+
+function save(transaction) {
+    showLoading();
     firebase.firestore().collection('transactions').add(transaction).then (() => {
         hideLoading();
         window.location.href = "../home/home.html";
     }). catch(() => {
         hideLoading();
         alert("Erro ao salvar transação")
+    }).catch(() => {
+        hideLoading();
+        alert("Erro ao atualizar transação");
+    });
+}
+
+function update(transaction) {
+    showLoading();
+    firebase.firestore().collection("transactions").doc(getTransactionUid()).update(transaction).then(() => {
+        hideLoading();
+        window.location.href = "../home/home.html";
     })
-    
 }
 
 function createTransaction() {
@@ -81,6 +153,7 @@ const form = {
     transactionType: () => document.getElementById('transaction-type'),
     transactionTypeRequiredError: () => document.getElementById('transaction-type-required-error'),
     typeExpense: () => document.getElementById('expense'),
+    typeIncome: () => document.getElementById('income'),
     currency: () => document.getElementById('currency'),
     description: () => document.getElementById('description')
 }
